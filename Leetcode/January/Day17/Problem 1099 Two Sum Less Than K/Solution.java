c060d73239b07390a9de4b61c4bc99895f4a976d
Abhishek Kulkarni <kulkarniabhi1995@gmail.com>
2021-01-17 20:15:03 -0500

    Day17

---
+++ Leetcode/January/Day17/Problem 1099 Two Sum Less Than K/Solution.java
@@ -0,0 +1,23 @@
+/*
+https://leetcode.com/problems/two-sum-less-than-k/solution/
+*/
+class Solution 
+{
+    public int twoSumLessThanK(int[] nums, int k) 
+    {
+        Arrays.sort(nums);
+        int answer = -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum < k) {
+                answer = Math.max(answer, sum);
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return answer;
+    }
+}
