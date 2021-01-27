bd3e7e11e98764768f3b04a8f3c5db3100d261d4
Abhishek Kulkarni <kulkarniabhi1995@gmail.com>
2021-01-26 20:13:56 -0500

    Day26

---
+++ Leetcode/January/Day26/Problem 26 Remove Duplicates from Sorted Array/Solution.java
@@ -0,0 +1,18 @@
+/*
+https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+*/
+class Solution 
+{
+   public int removeDuplicates(int[] nums) 
+   {
+        int i = nums.length > 0 ? 1 : 0;
+        for (int n : nums)
+        {   
+            if (n > nums[i-1])
+            {
+                nums[i++] = n;   
+            }
+        }
+        return i;
+    }
+}
