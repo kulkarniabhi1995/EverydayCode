86328dd6a202f7afc235691554c7f1fdc93e4d45
Abhishek Kulkarni <kulkarniabhi1995@gmail.com>
2021-01-28 20:43:00 -0500

    Day28

---
+++ Leetcode/January/Day28/Problem 155 Min Stack/Solution.java
@@ -0,0 +1,48 @@
+/*
+https://leetcode.com/problems/min-stack/
+*/
+class MinStack 
+{
+
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<int[]> minStack = new Stack<>();
+
+    public void push(int x) 
+    {
+        
+        stack.push(x);
+        if (minStack.isEmpty() || x < minStack.peek()[0]) 
+        {
+            minStack.push(new int[]{x, 1});
+        }
+        else if (x == minStack.peek()[0]) {
+            minStack.peek()[1]++;
+        }
+    }
+    
+    
+    public void pop() 
+    {
+        if (stack.peek().equals(minStack.peek()[0]))
+        {
+            minStack.peek()[1]--;
+        }
+        if (minStack.peek()[1] == 0) 
+        {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+    
+    
+    public int top() 
+    {
+        return stack.peek();
+    }
+
+    
+    public int getMin() 
+    {
+        return minStack.peek()[0];
+    }
+}
