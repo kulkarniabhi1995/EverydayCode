da19fb0f141132c7f7169e9db5e20108cf8c0036
Abhishek Kulkarni <kulkarniabhi1995@gmail.com>
2021-02-18 11:41:23 -0500

    Day18

---
+++ Leetcode/February/Day18/Problem 1564 Put Boxes Into the Warehouse I/Solution.java
@@ -0,0 +1,21 @@
+/*
+https://leetcode.com/problems/put-boxes-into-the-warehouse-i/
+*/
+class Solution 
+{
+    public int maxBoxesInWarehouse(int[] boxes, int[] warehouse) 
+    {
+        Arrays.sort(boxes);
+        int m = boxes.length;
+        int n = warehouse.length;
+        int res = 0; 
+        for(int i = 0;i < m & res<n;i++)
+        {
+            if(boxes[m-i-1] <= warehouse[res]) 
+            {
+                res++;
+            }
+        }
+        return res;
+    }
+}
---
+++ Leetcode/February/Day18/Problem 62 Unique Paths/Solution.java
@@ -0,0 +1,28 @@
+/*
+https://leetcode.com/problems/unique-paths/
+*/
+class Solution 
+{
+    public int uniquePaths(int m, int n) 
+    {
+         if(m == 1 || n == 1)
+            return 1;
+        m--;
+        n--;
+        if(m < n) 
+        {             
+            m = m + n;
+            n = m - n;
+            m = m - n;
+        }
+        long res = 1;
+        int j = 1;
+        for(int i = m+1; i <= m+n; i++, j++)
+        {  
+            res *= i;
+            res /= j;
+        }
+            
+        return (int)res;
+    }
+}
