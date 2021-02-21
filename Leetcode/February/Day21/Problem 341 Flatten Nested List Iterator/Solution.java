d0a7564dbe83a78fc265f046349a3181d0e0eae4
Abhishek Kulkarni <kulkarniabhi1995@gmail.com>
2021-02-21 11:53:33 -0500

    Day21

---
+++ Leetcode/February/Day21/Problem 341 Flatten Nested List Iterator/Solution.java
@@ -0,0 +1,72 @@
+/*
+https://leetcode.com/problems/flatten-nested-list-iterator/
+*/
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+import java.util.NoSuchElementException;
+
+public class NestedIterator implements Iterator<Integer> 
+{
+    private Deque<ListIterator<NestedInteger>> stackOfIterators = new ArrayDeque();
+    private Integer peeked = null;
+
+    public NestedIterator(List<NestedInteger> nestedList) 
+    {
+        stackOfIterators.addFirst(nestedList.listIterator());
+    }
+
+    private void setPeeked() 
+    {
+        if (peeked != null) return;
+        
+        while (!stackOfIterators.isEmpty()) 
+        {
+            if (!stackOfIterators.peekFirst().hasNext()) 
+            {
+                stackOfIterators.removeFirst();
+                continue;
+            }
+            NestedInteger next = stackOfIterators.peekFirst().next();
+            
+            if (next.isInteger()) 
+            {
+                peeked = next.getInteger();
+                return;
+            }
+            stackOfIterators.addFirst(next.getList().listIterator());
+        }        
+    }
+    
+
+    @Override
+    public Integer next() 
+    {
+        if (!hasNext()) throw new NoSuchElementException();
+        
+        Integer result = peeked;
+        peeked = null;
+        return result;
+    }
+
+    @Override
+    public boolean hasNext() 
+    {
+        setPeeked();
+        return peeked != null;
+    }
+}
