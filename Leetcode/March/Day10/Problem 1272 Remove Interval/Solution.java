a2887d0199dfdf718a98591cebea4d913c7209f3
Abhishek Kulkarni <kulkarniabhi1995@gmail.com>
2021-03-10 15:40:22 -0500

    Day10

---
+++ Leetcode/March/Day10/Problem 1272 Remove Interval/Solution.java
@@ -0,0 +1,30 @@
+/*
+https://leetcode.com/problems/remove-interval/
+*/
+class Solution 
+{
+    public List<List<Integer>> removeInterval(int[][] intervals, int[] toBeRemoved) 
+    {
+         List<List<Integer>> result = new ArrayList<>();
+        for (int[] interval : intervals) 
+        {
+        
+            if (interval[0] > toBeRemoved[1] || interval[1] < toBeRemoved[0]) 
+            {
+                result.add(Arrays.asList(interval[0], interval[1]));
+            }
+            else 
+            {
+                if (interval[0] < toBeRemoved[0]) 
+                {
+                    result.add(Arrays.asList(interval[0], toBeRemoved[0]));
+                }
+                if (interval[1] > toBeRemoved[1]) 
+                {
+                    result.add(Arrays.asList(toBeRemoved[1], interval[1]));
+                }
+            }
+        }
+        return result;    
+    }
+}
